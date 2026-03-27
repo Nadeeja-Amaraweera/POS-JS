1567f431235f71fba27516ef67970ed6792d2a4a
Nadeeja-Amaraweera <nadeejaamaraweera@gmail.com>
2026-03-27 17:32:08 +0530

    new

--- js/script.js
+++ js/script.js
@@ -4,8 +4,10 @@ var errorBox = document.querySelector(".error-msg-box");
 var errorCloseBtn = document.getElementById("error-close-btn");
 
 function showError(message) {
+    document.getElementById("error-text").textContent = message;
     errorBox.style.transform = "translate(-50%, -100%)";
     showErrorSlowly();
+    hideErrorAuto();
 }
 
 function hideError() {
@@ -20,7 +22,7 @@ function showErrorSlowly() {
 function hideErrorAuto() {
     setTimeout(function () {
         hideError();
-    },3000);
+    }, 3000);
 }
 
 if (errorCloseBtn) {
@@ -36,7 +38,6 @@ function login() {
         showpage("loginPage", "dashPage");
     } else {
         showError("Invalid username or password. Please try again.");
-        hideErrorAuto();
     }
 }
 
@@ -65,28 +66,28 @@ function loadPage(pageId) {
     if (target) {
         target.classList.remove("hidden");
         // Use 'flex' for the dashboard to keep the sidebar layout intact
-        target.style.display = (pageId === 'loginPage') ? "flex" : "flex"; 
+        target.style.display = (pageId === 'loginPage') ? "flex" : "flex";
     }
 
 
 
     const navLinks = document.querySelectorAll('nav a');
-    
+
     navLinks.forEach(link => {
         const pill = link.querySelector('.active-pill');
-        
+
         // Check if this link corresponds to the clicked page
         if (link.getAttribute('onclick').includes(pageId)) {
             // APPLY ACTIVE STYLES
             link.classList.add('bg-gray-300', 'text-gray-800');
             link.classList.remove('text-gray-300', 'hover:bg-white/10');
-            
+
             if (pill) pill.classList.remove('hidden'); // Show the pill
         } else {
             // RESET TO INACTIVE
             link.classList.remove('bg-gray-300', 'text-gray-800');
             link.classList.add('text-gray-300', 'hover:bg-white/10');
-            
+
             if (pill) pill.classList.add('hidden'); // Hide the pill
         }
     });
@@ -128,7 +129,7 @@ function loadOrderPage(pageId) {
 const passwordField = document.getElementById("passwordField");
 const icon = document.getElementById("eyeIcon");
 
-function togglePassword(){
+function togglePassword() {
     if (passwordField.type === "password") {
         passwordField.type = "text";
         icon.classList.replace("fa-eye", "fa-eye-slash");
@@ -150,3 +151,205 @@ function navigatePageBtn(btn) {
     const page = btn.querySelector("span").textContent.trim().toLowerCase();
     showpage("dashPage", page + "Page");
 }
+
+const customers = [];
+
+// Save Customer
+function saveCustomer() {
+    const name = document.getElementById("customerName").value;
+    const email = document.getElementById("customerEmail").value;
+    const phone = document.getElementById("customerPhone").value;
+
+    const customerId = "CUS" + String(customers.length + 1).padStart(3, '0');
+
+    if (!name || !email || !phone) {
+        showError("Please fill in all required fields.");
+        return;
+    } else {
+        // Proceed with saving the customer
+        customers.push({ customerId, name, email, phone });
+
+        Swal.fire({
+            icon: 'success',
+            title: 'Customer Added',
+            text: `The customer ${customerId} has been added successfully.`,
+            confirmButtonColor: '#3085d6'
+        });
+
+
+        // Clear the form after saving
+        console.log("Current Customers:", customers);
+        loadCustomers();
+    }
+}
+
+// Clear multiple fields by IDs
+function clearFields(...fieldIds) {
+    fieldIds.forEach(id => {
+        const field = document.getElementById(id);
+        if (field) {
+            field.value = "";
+        }
+    });
+}
+
+// Load Customers in Table
+function loadCustomers() {
+    const tableBody = document.getElementById("customerTableBody");
+    tableBody.innerHTML = ""; // Clear existing rows
+    customers.forEach(customer => {
+        const row = document.createElement("tr");
+        row.classList.add("hover:bg-gray-300");
+        row.innerHTML = `
+            <td class="px-4 py-2">${customer.customerId}</td>
+            <td class="px-4 py-2">${customer.name}</td>
+            <td class="px-4 py-2">${customer.email}</td>
+            <td class="px-4 py-2">${customer.phone}</td>
+            <td class="px-4 py-2">
+                <div class="flex gap-4">
+                    <div class="cursor-pointer text-indigo-500 hover:text-gray-700" onclick="editCustomer('${customer.customerId}')">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                        <path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32l8.4-8.4Z" />
+                        <path d="M5.25 5.25a3 3 0 0 0-3 3v10.5a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3V13.5a.75.75 0 0 0-1.5 0v5.25a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5V8.25a1.5 1.5 0 0 1 1.5-1.5h5.25a.75.75 0 0 0 0-1.5H5.25Z" />
+                        </svg>
+                    </div>
+
+                    <div class="cursor-pointer text-red-500 hover:text-gray-700" onclick="deleteCustomer('${customer.customerId}')">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                        <path fill-rule="evenodd" d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z" clip-rule="evenodd" />
+                        </svg>
+                    </div>
+                </div>
+            </td>
+        `;
+        tableBody.appendChild(row);
+    });
+}
+
+// Edit Customer
+function editCustomer(customerId) {
+    console.log("Edit customer with ID:", customerId);
+
+    customers.forEach(customer => {
+        if (customer.customerId === customerId) {
+            document.getElementById("customerName").value = customer.name;
+            document.getElementById("customerEmail").value = customer.email;
+            document.getElementById("customerPhone").value = customer.phone;
+            document.getElementById("customerId").value = customer.customerId;
+        }
+    });
+
+    Swal.fire({
+            icon: 'info',
+            title: 'Edit Mode',
+            text: `Editing ${customerId}. Update and click "Update Selected".`,
+            confirmButtonColor: '#3085d6'
+    });
+}
+
+// Update Customer
+function updateCustomer() {
+    const customerId = document.getElementById("customerId").value;
+    const name = document.getElementById("customerName").value;
+    const email = document.getElementById("customerEmail").value;
+    const phone = document.getElementById("customerPhone").value;
+    if (!customerId) {
+        showError("No customer selected for update.");
+        return;
+    } else if (!name || !email || !phone) {
+        showError("Please fill in all customer details.");
+        return;
+    } else {
+        const index = customers.findIndex(c => c.customerId === customerId);
+        if (index !== -1) {
+            customers[index] = { ...customers[index], name, email, phone };
+            loadCustomers();
+            Swal.fire({
+                icon: 'success',
+                title: 'Customer Updated',
+                text: `The customer ${customerId} has been updated successfully.`,
+                confirmButtonColor: '#3085d6'
+            });
+        }
+        loadCustomers();
+    }
+}
+
+// Delete Customer
+function deleteCustomer(customerId) {
+    console.log("Delete customer with ID:", customerId);
+
+    Swal.fire({
+        title: 'Delete Customer?',
+        text: 'This action cannot be undone.',
+        icon: 'question',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!',
+        cancelButtonText: 'No, cancel'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            // User clicked Yes
+            customers.splice(customers.findIndex(c => c.customerId === customerId), 1);
+            loadCustomers();
+
+            Swal.fire('Deleted!', 'The customer has been deleted.', 'success');
+        } else {
+            // User clicked No or closed the dialog
+            Swal.fire('Cancelled', 'The customer is safe', 'info');
+        }
+    });
+}
+
+// Confirm message before deleting customer
+function confirmDelete(customerId) {
+    const confirmation = confirm("Are you sure you want to delete this customer?");
+    if (confirmation) {
+        deleteCustomer(customerId);
+    }
+
+}
+
+// Search Customer
+function findCustomer() {
+    const query = document.getElementById("customerSearch").value.toLowerCase();
+    const tableBody = document.getElementById("customerTableBody");
+    tableBody.innerHTML = ""; // Clear existing rows
+    customers.forEach(customer => {
+        if (
+            customer.customerId.toLowerCase().includes(query) ||
+            customer.name.toLowerCase().includes(query) ||
+            customer.email.toLowerCase().includes(query) ||
+            customer.phone.includes(query)
+        ) {
+            const row = document.createElement("tr");
+            row.innerHTML = `
+                <td class="px-4 py-2">${customer.customerId}</td>
+                <td class="px-4 py-2">${customer.name}</td>
+                <td class="px-4 py-2">${customer.email}</td>
+                <td class="px-4 py-2">${customer.phone}</td>
+                <td class="px-4 py-2">
+                <div class="flex gap-4">
+                    <div class="cursor-pointer text-indigo-500 hover:text-gray-700" onclick="editCustomer('${customer.customerId}')">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                        <path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32l8.4-8.4Z" />
+                        <path d="M5.25 5.25a3 3 0 0 0-3 3v10.5a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3V13.5a.75.75 0 0 0-1.5 0v5.25a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5V8.25a1.5 1.5 0 0 1 1.5-1.5h5.25a.75.75 0 0 0 0-1.5H5.25Z" />
+                        </svg>
+                    </div>
+
+                    <div class="cursor-pointer text-red-500 hover:text-gray-700" onclick="deleteCustomer('${customer.customerId}')">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                        <path fill-rule="evenodd" d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z" clip-rule="evenodd" />
+                        </svg>
+                    </div>
+                </div>
+            </td>
+            `;
+            tableBody.appendChild(row);
+        } else {
+            showError("No customers found matching the search criteria.");
+        }
+    });
+}
+
