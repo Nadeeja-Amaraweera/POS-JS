9d6dc23b400bf0811fdb8355ae4f690022e7b283
Nadeeja-Amaraweera <nadeejaamaraweera@gmail.com>
2026-03-28 21:49:19 +0530

    Item Page

--- js/script.js
+++ js/script.js
@@ -303,15 +303,6 @@ function deleteCustomer(customerId) {
     });
 }
 
-// Confirm message before deleting customer
-function confirmDelete(customerId) {
-    const confirmation = confirm("Are you sure you want to delete this customer?");
-    if (confirmation) {
-        deleteCustomer(customerId);
-    }
-
-}
-
 // Search Customer
 function findCustomer() {
     const query = document.getElementById("customerSearch").value.toLowerCase();
@@ -371,6 +362,204 @@ function countCustomers() {
         document.getElementById("customerCount").textContent = "All Customers " + count;
     }
 }
-window.onload = countCustomers;
 
 
+// Save Item
+const items = [];
+function saveItem() {
+    const itemName = document.getElementById("itemName").value;
+    const itemPrice = document.getElementById("itemPrice").value;
+    const itemQuantity = document.getElementById("itemQuantity").value;
+
+    const itemId = "ITM" + String(items.length + 1).padStart(3, '0');
+
+    if (!itemName || !itemPrice || !itemQuantity) {
+        showError("Please fill in all required fields.");
+        return;
+    } else {
+        // Proceed with saving the item
+        items.push({ itemId, itemName, itemPrice, itemQuantity });
+
+        Swal.fire({
+            icon: 'success',
+            title: 'Item Added',
+            text: `The item ${itemId} has been added successfully.`,
+            confirmButtonColor: '#3085d6'
+        });
+
+
+        // Clear the form after saving
+        console.log("Current Items:", items);
+        loadItems();
+    }
+}
+
+// Load Items in Table
+function loadItems() {
+    const tableBody = document.getElementById("itemTableBody");
+    tableBody.innerHTML = ""; // Clear existing rows
+    items.forEach(item => {
+        const row = document.createElement("tr");
+        row.classList.add("hover:bg-gray-300");
+        row.innerHTML = `
+            <td class="px-4 py-2">${item.itemId}</td>
+            <td class="px-4 py-2">${item.itemName}</td>
+            <td class="px-4 py-2">${item.itemPrice}</td>
+            <td class="px-4 py-2">${item.itemQuantity}</td>
+            <td class="px-4 py-2">
+                <div class="flex gap-4">
+                    <div class="cursor-pointer text-indigo-500 hover:text-gray-700" onclick="editItem('${item.itemId}')">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                        <path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32l8.4-8.4Z" />
+                        <path d="M5.25 5.25a3 3 0 0 0-3 3v10.5a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3V13.5a.75.75 0 0 0-1.5 0v5.25a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5V8.25a1.5 1.5 0 0 1 1.5-1.5h5.25a.75.75 0 0 0 0-1.5H5.25Z" />
+                        </svg>
+                    </div>
+
+                    <div class="cursor-pointer text-red-500 hover:text-gray-700" onclick="deleteItem('${item.itemId}')">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                        <path fill-rule="evenodd" d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z" clip-rule="evenodd" />
+                        </svg>
+                    </div>
+                </div>
+            </td>
+        `;
+        tableBody.appendChild(row);
+    });
+    countItems();
+}
+
+// Edit Item
+function editItem(itemId) {
+    console.log("Edit item with ID:", itemId);
+
+    items.forEach(item => {
+        if (item.itemId === itemId) {
+            document.getElementById("itemName").value = item.itemName;
+            document.getElementById("itemPrice").value = item.itemPrice;
+            document.getElementById("itemQuantity").value = item.itemQuantity;
+            document.getElementById("itemId").value = item.itemId;
+        }
+    });
+
+    Swal.fire({
+            icon: 'info',
+            title: 'Edit Mode',
+            text: `Editing ${itemId}. Update and click "Update Selected".`,
+            confirmButtonColor: '#3085d6'
+    });
+}
+
+// Update Item
+function updateItem() {
+    const itemId = document.getElementById("itemId").value;
+    const itemName = document.getElementById("itemName").value;
+    const itemPrice = document.getElementById("itemPrice").value;
+    const itemQuantity = document.getElementById("itemQuantity").value;
+    if (!itemId) {
+        showError("No item selected for update.");
+        return;
+    } else if (!itemName || !itemPrice || !itemQuantity) {
+        showError("Please fill in all item details.");
+        return;
+    } else {
+        const index = items.findIndex(i => i.itemId === itemId);
+        if (index !== -1) {
+            items[index] = { ...items[index], itemName, itemPrice, itemQuantity };
+            loadItems();
+            Swal.fire({
+                icon: 'success',
+                title: 'Item Updated',
+                text: `The item ${itemId} has been updated successfully.`,
+                confirmButtonColor: '#3085d6'
+            });
+        }
+        loadItems();
+    }
+}
+
+// Delete Item
+function deleteItem(itemId) {
+    console.log("Delete item with ID:", itemId);
+
+    Swal.fire({
+        title: 'Delete Item?',
+        text: 'This action cannot be undone.',
+        icon: 'question',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!',
+        cancelButtonText: 'No, cancel'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            // User clicked Yes
+            items.splice(items.findIndex(i => i.itemId === itemId), 1);
+            loadItems();
+
+            Swal.fire('Deleted!', 'The item has been deleted.', 'success');
+        } else {
+            // User clicked No or closed the dialog
+            Swal.fire('Cancelled', 'The item is safe', 'info');
+        }
+    });
+}
+
+// Count Items
+function countItems() {
+    const count = items.length;
+    if (count === 0) {
+        document.getElementById("itemCount").textContent = "No items added";
+    } else {
+        document.getElementById("itemCount").textContent = "All Items " + count;
+    }
+}
+
+// Search Item
+function findItem() {
+    const query = document.getElementById("itemSearch").value.toLowerCase();
+    // const tableBody = document.getElementById("customerTableBody");
+    // tableBody.innerHTML = ""; // Clear existing rows
+    const resultDiv = document.getElementById("ItemSearchResultArea"); 
+
+    let results = "";
+    let found = false;
+
+    items.forEach(item => {
+        if (
+            item.itemId.toLowerCase().includes(query) ||
+            item.itemName.toLowerCase().includes(query) ||
+            item.itemPrice.toString().includes(query) ||
+            item.itemQuantity.toString().includes(query)
+        ) {
+            found = true;
+            results += `
+                <div class="flex justify-between items-center flex-wrap border p-3 rounded-lg">
+                                <div>
+                                    <p class="font-medium">${item.itemName}</p>
+                                    <p class="text-xs">ID: ${item.itemId} | Price: $${item.itemPrice} | Qty: ${item.itemQuantity}</p>
+                                </div>
+                                <button id="quickUnregisterFromSearch"
+                                    class="bg-gray-300 text-gray-700 px-3 py-1 rounded-lg text-xs cursor-pointer hover:bg-gray-400 hover:text-gray-900 transition duration-200 ease-in-out hover:scale-102"
+                                    onclick="editItem('${item.itemId}')">
+                                    <i class="fas fa-user-edit"></i> Edit
+                                </button>
+                                <button id="quickUnregisterFromSearch"
+                                    class="bg-rose-100 text-rose-700 px-3 py-1 rounded-lg text-xs cursor-pointer hover:bg-rose-200 transition duration-200 ease-in-out hover:scale-102"
+                                    onclick="deleteItem('${item.itemId}')">
+                                    <i class="fas fa-user-minus"></i> Delete
+                                </button>
+                                
+                            </div>
+            `;
+            console.log("Item found:", item.itemId);
+        }
+    });
+
+    if (found) {
+        resultDiv.classList.remove("hidden");
+        resultDiv.innerHTML = results;
+    } else {
+        resultDiv.classList.remove("hidden"); 
+        resultDiv.innerHTML = "<p class='text-sm text-gray-500'>No items found matching the search criteria.</p>";
+    }
+}
